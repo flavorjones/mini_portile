e77b90938828980e670278e1c3bf83a62515a7fd
Lars Kanis <lars@greiz-reinsdorf.de>
2015-06-27 22:22:37 +0200

    Take configure_options literally, instead of simply join with a space.
    This allows the use of characters within the configure_options, that are
    otherwise interpreted by the shell, in particular spaces.
    
    It explicit avoids shellescape because stdlib's shellwords follows Unix rules
    only, for shell escaping, but doesn't work on Windows.
    
    This is syntax compatible and working on Ruby-1.8.7. However it prints, as a
    compromise, configure output to stdout/err instead of writing to the log file,
    in this case.
    
    This slightly changes the semantics of API, so that it could break some build
    scripts, if they do shell escaping to configure options on their own. This is
    in particular nokogiri. So this patch is probably something for a 0.7 series.

--- lib/mini_portile/mini_portile.rb
+++ lib/mini_portile/mini_portile.rb
@@ -44,8 +44,8 @@ class MiniPortile
     begin
       @patch_files.each do |full_path|
         next unless File.exists?(full_path)
-        output "Running git apply with #{full_path}..."
-        execute('patch', %Q(git apply #{full_path}))
+        output "Running git apply with #{full_path}... "
+        execute('patch', %w(git apply) + [full_path], :initial_message => false)
       end
     ensure
       ENV['GIT_DIR'] = old_git
@@ -60,10 +60,10 @@ class MiniPortile
     return if configured?
 
     md5_file = File.join(tmp_path, 'configure.md5')
-    digest   = Digest::MD5.hexdigest(computed_options)
+    digest   = Digest::MD5.hexdigest(computed_options.to_s)
     File.open(md5_file, "w") { |f| f.write digest }
 
-    execute('configure', %Q(sh configure #{computed_options}))
+    execute('configure', %w(sh configure) + computed_options)
   end
 
   def compile
@@ -90,7 +90,7 @@ class MiniPortile
     md5_file  = File.join(tmp_path, 'configure.md5')
 
     stored_md5  = File.exist?(md5_file) ? File.read(md5_file) : ""
-    current_md5 = Digest::MD5.hexdigest(computed_options)
+    current_md5 = Digest::MD5.hexdigest(computed_options.to_s)
 
     (current_md5 == stored_md5) && newer?(makefile, configure)
   end
@@ -186,7 +186,7 @@ private
     [
       configure_options,     # customized or default options
       configure_prefix,      # installation target
-    ].flatten.join(' ')
+    ].flatten
   end
 
   def log_file(action)
@@ -263,14 +263,29 @@ private
     end
   end
 
-  def execute(action, command)
+  def execute(action, command, options={})
     log        = log_file(action)
     log_out    = File.expand_path(log)
-    redirected = command << " >#{log_out} 2>&1"
 
     Dir.chdir work_path do
-      message "Running '#{action}' for #{@name} #{@version}... "
-      system redirected
+      if options.fetch(:initial_message){ true }
+        message "Running '#{action}' for #{@name} #{@version}... "
+      end
+
+      if Process.respond_to?(:spawn)
+        args = [command].flatten + [{[:out, :err]=>[log_out, "w"]}]
+        pid = spawn(*args)
+        Process.wait(pid)
+      else
+        # Ruby-1.8 compatibility:
+        if command.kind_of?(Array)
+          system(*command)
+        else
+          redirected = command << " >#{log_out} 2>&1"
+          system(redirected)
+        end
+      end
+
       if $?.success?
         output "OK"
         return true
