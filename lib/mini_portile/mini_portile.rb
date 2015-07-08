3ff8b8ace1cdea5070185cd6411bcffb4009833f
Lars Kanis <lars@greiz-reinsdorf.de>
2015-07-08 21:52:48 +0200

    Do not clobber the previously written content of a log file.
    In the "patch" task execute() is called many times, so log should be
    written in append mode.
    
    Based on the work of Akinori MUSHA<knu@idaemons.org> in
        https://github.com/flavorjones/mini_portile/pull/34

--- lib/mini_portile/mini_portile.rb
+++ lib/mini_portile/mini_portile.rb
@@ -17,6 +17,7 @@ class MiniPortile
     @target = 'ports'
     @files = []
     @patch_files = []
+    @log_files = {}
     @logger = STDOUT
 
     @original_host = @host = detect_host
@@ -190,7 +191,10 @@ private
   end
 
   def log_file(action)
-    File.join(tmp_path, "#{action}.log")
+    @log_files[action] ||=
+      File.expand_path("#{action}.log", tmp_path).tap { |file|
+        File.unlink(file) if File.exist?(file)
+      }
   end
 
   def tar_exe
@@ -271,8 +275,7 @@ private
   end
 
   def execute(action, command, options={})
-    log        = log_file(action)
-    log_out    = File.expand_path(log)
+    log_out    = log_file(action)
 
     Dir.chdir work_path do
       if options.fetch(:initial_message){ true }
@@ -280,7 +283,7 @@ private
       end
 
       if Process.respond_to?(:spawn)
-        args = [command].flatten + [{[:out, :err]=>[log_out, "w"]}]
+        args = [command].flatten + [{[:out, :err]=>[log_out, "a"]}]
         pid = spawn(*args)
         Process.wait(pid)
       else
