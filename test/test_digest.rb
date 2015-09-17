cd16acf4b83575b465d162ceba688408ab8e4228
JT Archie <jtarchie@gmail.com>
2015-09-17 16:23:33 -0400

    support gpg signature verification of download resources
    [#103443076]
    https://www.pivotaltracker.com/story/show/103443076
    
    Signed-off-by: David Morhovich <dmorhovich@pivotal.io>

--- test/test_digest.rb
+++ test/test_digest.rb
@@ -65,5 +65,85 @@ class TestDigest < TestCase
   def test_wrong_md5
     download_with_wrong_digest(:md5)
   end
+
+  def test_with_valid_gpg_signature
+    key       = "A1C052F8"
+    signature = <<-SIGNATURE
+-----BEGIN PGP SIGNATURE-----
+Version: GnuPG v2
+
+iQEcBAABCAAGBQJV002uAAoJEFIKmZOhwFL41AcH/2VX1/5mD3dAUXfDaYMG92IV
+aA8vHlsvXpCEPfCYBnPGYYFa/P0qPyw6hsWXZhWEGEm+BqZK6dWCLFaxTVTtsjOE
+vhSR+LL+FNxYmGbK2lYq61PDDL45x5Qnhy3WK1e40F7CqmElSfMOjLuCNC7xR9Jc
+zAZ014ADQ5yfH+Ma40K997AxZeCVGU+A5IEHGoZ2i8pyqx0Jhh6cbpC18yHu5ciN
+0o4E4cLSFFckYB3FnUpDowRonBDNUpDRJVKMo5cvvskc/GWVUVomPuWyNGFPPmMJ
+aySUQcOvO67Z14d9E9ziX/E24KWl6xRymmy9VhzawgSmf//3yZVaD6C/8om3qMw=
+=zjw3
+-----END PGP SIGNATURE-----
+    SIGNATURE
+
+    @recipe.files << {
+      :url => "http://nginx.org/download/nginx-1.9.4.tar.gz",
+      :gpg => {
+        :key => key,
+        :signature => signature
+      }
+    }
+    @recipe.download
+  end
+
+  def test_with_invalid_gpg_signature
+    key       = "A1C052F8"
+    signature = <<-SIGNATURE
+-----BEGIN PGP SIGNATURE-----
+Version: GnuPG v2
+
+iQEcBAABCAAGBQJV002uAAoJEFIKmZOhwFL41AcH/2VX1/5mD3dAUXfDaYMG92IV
+AA8vHlsvXpCEPfCYBnPGYYFa/P0qPyw6hsWXZhWEGEm+BqZK6dWCLFaxTVTtsjOE
+vhSR+LL+FNxYmGbK2lYq61PDDL45x5Qnhy3WK1e40F7CqmElSfMOjLuCNC7xR9Jc
+zAZ014ADQ5yfH+Ma40K997AxZeCVGU+A5IEHGoZ2i8pyqx0Jhh6cbpC18yHu5ciN
+0o4E4cLSFFckYB3FnUpDowRonBDNUpDRJVKMo5cvvskc/GWVUVomPuWyNGFPPmMJ
+aySUQcOvO67Z14d9E9ziX/E24KWl6xRymmy9VhzawgSmf//3yZVaD6C/8om3qMw=
+=zjw3
+-----END PGP SIGNATURE-----
+    SIGNATURE
+
+    @recipe.files << {
+      :url => "http://nginx.org/download/nginx-1.9.4.tar.gz",
+      :gpg => {
+        :key => key,
+        :signature => signature
+      }
+    }
+    exception = assert_raise(RuntimeError){ @recipe.download }
+    assert_equal("signature mismatch", exception.message)
+  end
+
+  def test_with_invalid_key
+    key       = "thisisaninvalidkey"
+    signature = <<-SIGNATURE
+-----BEGIN PGP SIGNATURE-----
+Version: GnuPG v2
+
+iQEcBAABCAAGBQJV002uAAoJEFIKmZOhwFL41AcH/2VX1/5mD3dAUXfDaYMG92IV
+aA8vHlsvXpCEPfCYBnPGYYFa/P0qPyw6hsWXZhWEGEm+BqZK6dWCLFaxTVTtsjOE
+vhSR+LL+FNxYmGbK2lYq61PDDL45x5Qnhy3WK1e40F7CqmElSfMOjLuCNC7xR9Jc
+zAZ014ADQ5yfH+Ma40K997AxZeCVGU+A5IEHGoZ2i8pyqx0Jhh6cbpC18yHu5ciN
+0o4E4cLSFFckYB3FnUpDowRonBDNUpDRJVKMo5cvvskc/GWVUVomPuWyNGFPPmMJ
+aySUQcOvO67Z14d9E9ziX/E24KWl6xRymmy9VhzawgSmf//3yZVaD6C/8om3qMw=
+=zjw3
+-----END PGP SIGNATURE-----
+    SIGNATURE
+
+    @recipe.files << {
+      :url => "http://nginx.org/download/nginx-1.9.4.tar.gz",
+      :gpg => {
+        :key => key,
+        :signature => signature
+      }
+    }
+    exception = assert_raise(RuntimeError){ @recipe.download }
+    assert_equal("key download failed", exception.message)
+  end
 end
 
