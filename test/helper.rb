7c8f61227349c809ef2de9758bf4b2029756116d
Mike Dalessio <mike.dalessio@gmail.com>
2016-01-06 11:29:33 -0500

    convert to minitest

--- test/helper.rb
+++ test/helper.rb
@@ -1,4 +1,6 @@
-require 'test/unit'
+require 'minitest/autorun'
+require 'minitest/unit'
+require 'minitest/hooks/test'
 require 'webrick'
 require 'fileutils'
 require 'zlib'
@@ -7,43 +9,43 @@ require 'fileutils'
 require 'erb'
 require 'mini_portile2'
 
-class TestCase < Test::Unit::TestCase
-  class << self
-    HTTP_PORT = 23523
+class TestCase < Minitest::Test
+  include Minitest::Hooks
 
-    attr_accessor :webrick
+  HTTP_PORT = 23523
 
-    def start_webrick(path)
-      @webrick = WEBrick::HTTPServer.new(:Port => HTTP_PORT, :DocumentRoot => path).tap do |w|
-        Thread.new do
-          w.start
-        end
-        until w.status==:Running
-          sleep 0.1
-        end
+  attr_accessor :webrick
+
+  def start_webrick(path)
+    @webrick = WEBrick::HTTPServer.new(:Port => HTTP_PORT, :DocumentRoot => path).tap do |w|
+      Thread.new do
+        w.start
+      end
+      until w.status==:Running
+        sleep 0.1
       end
     end
+  end
 
-    def stop_webrick
-      if w=@webrick
-        w.shutdown
-        until w.status==:Stop
-          sleep 0.1
-        end
+  def stop_webrick
+    if w=@webrick
+      w.shutdown
+      until w.status==:Stop
+        sleep 0.1
       end
     end
+  end
 
-    def create_tar(tar_path, assets_path)
-      FileUtils.mkdir_p(File.dirname(tar_path))
-      Zlib::GzipWriter.open(tar_path) do |fdtgz|
-        Dir.chdir(assets_path) do
-          Archive::Tar::Minitar.pack("test mini portile-1.0.0", fdtgz)
-        end
+  def create_tar(tar_path, assets_path)
+    FileUtils.mkdir_p(File.dirname(tar_path))
+    Zlib::GzipWriter.open(tar_path) do |fdtgz|
+      Dir.chdir(assets_path) do
+        Archive::Tar::Minitar.pack("test mini portile-1.0.0", fdtgz)
       end
     end
+  end
 
-    def work_dir(r=recipe)
-      "tmp/#{r.host}/ports/#{r.name}/#{r.version}/#{r.name}-#{r.version}"
-    end
+  def work_dir(r=recipe)
+    "tmp/#{r.host}/ports/#{r.name}/#{r.version}/#{r.name}-#{r.version}"
   end
 end
--- test/test_digest.rb
+++ test/test_digest.rb
@@ -1,49 +1,45 @@
 require File.expand_path('../helper', __FILE__)
 
 class TestDigest < TestCase
-  class << self
-    attr_accessor :assets_path
-    attr_accessor :tar_path
-    attr_accessor :recipe
+  attr :assets_path, :tar_path, :recipe
 
-    def startup
-      @assets_path = File.expand_path("../assets", __FILE__)
-      @tar_path = File.expand_path("../../tmp/test-digest-1.0.0.tar.gz", __FILE__)
+  def before_all
+    super
+    @assets_path = File.expand_path("../assets", __FILE__)
+    @tar_path = File.expand_path("../../tmp/test-digest-1.0.0.tar.gz", __FILE__)
 
-      # remove any previous test files
-      FileUtils.rm_rf("tmp")
+    # remove any previous test files
+    FileUtils.rm_rf("tmp")
 
-      create_tar(@tar_path, @assets_path)
-      start_webrick(File.dirname(@tar_path))
-    end
+    create_tar(@tar_path, @assets_path)
+    start_webrick(File.dirname(@tar_path))
+  end
 
-    def shutdown
-      stop_webrick
-      # leave test files for inspection
-    end
+  def after_all
+    stop_webrick
+    # leave test files for inspection
   end
 
   def setup
-    # remove any download files
+    super
     FileUtils.rm_rf("ports/archives")
-
     @recipe = MiniPortile.new("test-digest", "1.0.0")
   end
 
   def download_with_digest(key, klass)
     @recipe.files << {
-      :url => "http://localhost:#{self.class.webrick.config[:Port]}/#{ERB::Util.url_encode(File.basename(self.class.tar_path))}",
-      key => klass.file(self.class.tar_path).hexdigest,
+      :url => "http://localhost:#{webrick.config[:Port]}/#{ERB::Util.url_encode(File.basename(tar_path))}",
+      key => klass.file(tar_path).hexdigest,
     }
     @recipe.download
   end
 
   def download_with_wrong_digest(key)
     @recipe.files << {
-      :url => "http://localhost:#{self.class.webrick.config[:Port]}/#{ERB::Util.url_encode(File.basename(self.class.tar_path))}",
+      :url => "http://localhost:#{webrick.config[:Port]}/#{ERB::Util.url_encode(File.basename(tar_path))}",
       key => "0011223344556677",
     }
-    assert_raise(RuntimeError){ @recipe.download }
+    assert_raises(RuntimeError){ @recipe.download }
   end
 
   def test_sha256
@@ -70,3 +66,4 @@ class TestDigest < TestCase
     download_with_wrong_digest(:md5)
   end
 end
+
