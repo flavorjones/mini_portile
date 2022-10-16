03835b2ede4139a9abd6144d14d842ccbf17cabf
Jeffrey Clark <h0tw1r3@users.noreply.github.com>
2022-10-16 15:48:58 -0500

    fix: git patch when config present

--- lib/mini_portile2/mini_portile.rb
+++ lib/mini_portile2/mini_portile.rb
@@ -101,7 +101,7 @@ class MiniPortile
           message "Running git apply with #{file}... "
           # By --work-tree=. git-apply uses the current directory as
           # the project root and will not search upwards for .git.
-          execute('patch', ["git", "--git-dir=.", "--work-tree=.", "apply", "--whitespace=warn", file], :initial_message => false)
+          execute('patch', ["git", "--git-dir=#{tmp_path}", "--work-tree=.", "apply", "--whitespace=warn", file], :initial_message => false)
         }
       when which('patch')
         lambda { |file|
